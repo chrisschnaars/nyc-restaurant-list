b40505d1391f2905b43565fdaa628d1743365a27
chrisschnaars <schnaarschris@gmail.com>
2020-03-13 10:38:18 -0500

    Update favicon and html root file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import "./scss/main.scss";
-require('dotenv').config();
+require("dotenv").config();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector(".site-container"));
